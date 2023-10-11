359f0d51798e4b566e80a159b8821f315cbe40c2
DatGuta <wtsdatho@gmail.com>
2023-10-11 08:12:38 +0700

    Initial commit

---
+++ src/main/java/DATN_BookWorms/DatnBookWormsApplication.java
@@ -0,0 +1,13 @@
+package DATN_BookWorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnBookWormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnBookWormsApplication.class, args);
+	}
+
+}
