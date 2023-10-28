8be06397a4ff81f29e8e23cfa73351db4156b64e
Zendyy00 <thienlcps20036@fpt.edu.vn>
2023-10-28 17:23:31 +0700

    zendyy_product_detail
    up lần 2

--- src/main/java/com/poly/DATN_BookWorms/entities/Books.java
+++ src/main/java/com/poly/DATN_BookWorms/entities/Books.java
@@ -6,6 +6,8 @@ package com.poly.DATN_BookWorms.entities;
 import java.io.Serializable;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -75,6 +77,7 @@ public class Books implements Serializable {
 
     //--- ENTITY LINKS ( RELATIONSHIP )
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Cart> listOfCart ; 
 
     @ManyToOne
@@ -82,15 +85,19 @@ public class Books implements Serializable {
     private Shoponlines shoponlines ; 
 
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Imagebooks> listOfImagebooks ; 
 
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Detailbookings> listOfDetailbookings ; 
 
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Typebooks> listOfTypebooks ; 
 
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Writers> listOfWriters ; 
 
     @ManyToOne
@@ -98,6 +105,7 @@ public class Books implements Serializable {
     private Publishingcompanies publishingcompanies ; 
 
     @OneToMany(mappedBy="books")
+    @JsonIgnore
     private List<Hassales> listOfHassales ; 
 
 
--- src/main/java/com/poly/DATN_BookWorms/entities/Categories.java
+++ src/main/java/com/poly/DATN_BookWorms/entities/Categories.java
@@ -8,6 +8,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -54,6 +56,7 @@ public class Categories implements Serializable {
 
 	// --- ENTITY LINKS ( RELATIONSHIP )
 	@OneToMany(mappedBy = "categories")
+	@JsonIgnore
 	private List<Typebooks> listOfTypebooks;
 
 	// --- toString specific method
