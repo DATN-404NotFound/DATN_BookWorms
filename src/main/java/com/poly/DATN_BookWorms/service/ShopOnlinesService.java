fbe9c83f230f1d4b6244edd7ec63d50ba2539d11
Đỗ Quang Tùng <99242911+tungcfpro@users.noreply.github.com>
2023-11-02 15:33:21 +0700

    Shop (error)

---
+++ src/main/java/com/poly/DATN_BookWorms/service/ShopOnlinesService.java
@@ -0,0 +1,10 @@
+package com.poly.DATN_BookWorms.service;
+
+import com.poly.DATN_BookWorms.entities.Shoponlines;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ShopOnlinesService {
+
+    Shoponlines findById(Integer cid);
+}
