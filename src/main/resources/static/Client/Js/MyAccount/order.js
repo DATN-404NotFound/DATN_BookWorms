9fb0cd8f3b854d25ab6dd42df7de2ffdc98e3b5c
Camvy1804 <92627708+Camvy1804@users.noreply.github.com>
2023-11-02 15:44:29 +0700

    chỉnh sửa giao diện my account

---
+++ src/main/resources/static/Client/Js/MyAccount/order.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Bắt sự kiện khi tab được chọn
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href"); // Lấy href của tab đã được chọn
+        // Thực hiện các thay đổi cần thiết
+    });
+});
