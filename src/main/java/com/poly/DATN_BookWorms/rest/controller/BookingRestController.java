2d12ad51d2ea60b672f61cf307cd7cd208b5e58b
Đỗ Quang Tùng <99242911+tungcfpro@users.noreply.github.com>
2023-12-14 13:33:36 +0700

    Update download PDF

--- src/main/java/com/poly/DATN_BookWorms/rest/controller/BookingRestController.java
+++ src/main/java/com/poly/DATN_BookWorms/rest/controller/BookingRestController.java
@@ -32,92 +32,123 @@ import java.util.List;
 @RestController
 @RequestMapping("/rest/bookings")
 public class BookingRestController {
-	@Autowired
-	BookingService bookingService;
-	
-	@Autowired
-	AccountService accountService;
+    @Autowired
+    BookingService bookingService;
 
-	@Autowired
-	HttpServletRequest httpServletRequest;
-	@Autowired
-	DetailBookingService detailBookingService;
-	@Autowired
-	CRC32_SHA256 crc;
-	@PostMapping()
-	public Bookings create(@RequestBody JsonNode bookingData, HttpServletRequest request) {
-		return bookingService.create(bookingData);
-	
-	}
-	@GetMapping("")
-	public List<Bookings> getAll() {
-		return bookingService.findAll();	
-	}
-	// @GetMapping
-	// public List<Bookings> getAll(){
+    @Autowired
+    AccountService accountService;
 
-	// 	return bookingService.findAll();
-	// }
-	@GetMapping("/user")
-	public List<Bookings> booking(){
+    @Autowired
+    HttpServletRequest httpServletRequest;
+    @Autowired
+    DetailBookingService detailBookingService;
+    @Autowired
+    CRC32_SHA256 crc;
 
-		return bookingService.findByUserId(crc.getCodeCRC32C(httpServletRequest.getRemoteUser()));
-	}
-	@GetMapping("/findByBookingId/{bookingId}")
-	public List<Detailbookings> findByBookingId(@PathVariable String bookingId) {
-		return detailBookingService.findByBookingId(bookingId);
-	}
-	@GetMapping("/findByBookingIdDetail/{bookingId}")
-	public Bookings findByBookingIdDetail(@PathVariable String bookingId) {
-		return bookingService.findByBookingId(bookingId);
-	}
-	@GetMapping("/generate/{bookingid}")
-	public ResponseEntity<byte[]> generatePdf(@PathVariable String bookingid) {
-		try {
-			Rectangle pageSize = new Rectangle(PageSize.LETTER);
+    @PostMapping()
+    public Bookings create(@RequestBody JsonNode bookingData, HttpServletRequest request) {
+        return bookingService.create(bookingData);
 
-			float marginLeft = 0.5f;
-			float marginRight = 0.5f;
-			float marginTop = 0.5f;
-			float marginBottom = 0.5f;
+    }
 
-			Document document = new Document(pageSize, marginLeft, marginRight, marginTop, marginBottom);
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			PdfWriter.getInstance(document, baos);
-			document.open();
-			BaseFont unicodeFont = BaseFont.createFont("C:\\Windows\\Fonts\\Arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-			com.itextpdf.text.Font titleFont = new com.itextpdf.text.Font(unicodeFont, 18, com.itextpdf.text.Font.BOLD, BaseColor.BLACK);
-			com.itextpdf.text.Font contentFont = new com.itextpdf.text.Font(unicodeFont, 12, BaseColor.BLACK.getRGB());
-			Bookings bookings = bookingService.findByBookingId(bookingid);
-			List<Detailbookings> detailbookings = detailBookingService.findByBookingId(bookingid);
+    @GetMapping("")
+    public List<Bookings> getAll() {
+        return bookingService.findAll();
+    }
+    // @GetMapping
+    // public List<Bookings> getAll(){
 
-			document.add(new Paragraph(detailbookings.get(0).getBooks().getShoponlines().getShopname().toUpperCase() + "STORE", titleFont));
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			document.add(new Paragraph("Ngày: "+new Date(), contentFont));
-			document.add(new Paragraph("Mã hóa đơn: " + bookings.getBookingid() , contentFont));
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			document.add(new Paragraph("Địa chỉ: Phường Nhà Mát, Thành phố Bạc Liêu, Bạc Liêu", contentFont));
-			document.add(new Paragraph("Số điện thoại: 0378977287", contentFont));
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			document.add(new Paragraph("Sản phẩm             Số lượng      Giá", contentFont));
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			for (Detailbookings detailbooking : detailbookings) {
-				document.add(new Paragraph(detailbooking.getBooks().getBookname()+"          "+detailbooking.getQuantity()+"                  "+detailbooking.getBooks().getPrice(), contentFont));
-			}
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			document.add(new Paragraph("Tổng cộng:                             "+bookings.getCost(), contentFont));
-			document.add(new Paragraph("----------------------------------------", contentFont));
-			document.add(new Paragraph("Cảm ơn quý khách đã mua hàng!", contentFont));
-			document.close();
-			byte[] pdfContents = baos.toByteArray();
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.parseMediaType("application/pdf"));
-			headers.setContentDispositionFormData("inline", "example.pdf");
-			return new ResponseEntity<>(pdfContents, headers, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+    // 	return bookingService.findAll();
+    // }
+    @GetMapping("/user")
+    public List<Bookings> booking() {
+
+        return bookingService.findByUserId(crc.getCodeCRC32C(httpServletRequest.getRemoteUser()));
+    }
+
+    @GetMapping("/findByBookingId/{bookingId}")
+    public List<Detailbookings> findByBookingId(@PathVariable String bookingId) {
+        return detailBookingService.findByBookingId(bookingId);
+    }
+
+    @GetMapping("/findByBookingIdDetail/{bookingId}")
+    public Bookings findByBookingIdDetail(@PathVariable String bookingId) {
+        return bookingService.findByBookingId(bookingId);
+    }
+
+    @GetMapping("/generate/{bookingid}")
+    public ResponseEntity<byte[]> generatePdf(@PathVariable String bookingid) {
+        try {
+            Rectangle pageSize = new Rectangle(PageSize.A6);
+
+            float marginLeft = 0.5f;
+            float marginRight = 0.5f;
+            float marginTop = 0.5f;
+            float marginBottom = 0.5f;
+
+            Document document = new Document(pageSize, marginLeft, marginRight, marginTop, marginBottom);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PdfWriter.getInstance(document, baos);
+            document.open();
+            BaseFont unicodeFont = BaseFont.createFont("C:\\Windows\\Fonts\\Arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            com.itextpdf.text.Font titleFont = new com.itextpdf.text.Font(unicodeFont, 18, com.itextpdf.text.Font.BOLD, BaseColor.BLACK);
+            com.itextpdf.text.Font contentFont = new com.itextpdf.text.Font(unicodeFont, 12, BaseColor.BLACK.getRGB());
+            Bookings bookings = bookingService.findByBookingId(bookingid);
+            List<Detailbookings> detailbookings = detailBookingService.findByBookingId(bookingid);
+
+            // Create a centered Paragraph for the title
+            Paragraph titleParagraph = new Paragraph(detailbookings.get(0).getBooks().getShoponlines().getShopname().toUpperCase() + " STORE", titleFont);
+            titleParagraph.setAlignment(Element.ALIGN_CENTER);
+            document.add(titleParagraph);
+
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            String inputString = "    ------------------------------------------------------------------    ";
+
+            System.out.println("Số lượng dấu gạch ngang: " + inputString.length());
+            document.add(new Paragraph("    Ngày: " + new Date(), contentFont));
+            document.add(new Paragraph("    Mã hóa đơn: " + bookings.getBookingid(), contentFont));
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            document.add(new Paragraph("    Địa chỉ: Phường Nhà Mát, Thành phố Bạc Liêu", contentFont));
+            document.add(new Paragraph("    Bạc Liêu", contentFont));
+            document.add(new Paragraph("    Số điện thoại: 0378977287", contentFont));
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            document.add(new Paragraph("    Địa chỉ: Phường Nhà Mát, Thành phố Bạc Liêu", contentFont));
+            document.add(new Paragraph("    Bạc Liêu", contentFont));
+            document.add(new Paragraph("    Số điện thoại: 0378977287", contentFont));
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+
+            // Center-align the product details table
+            document.add(new Paragraph("            Số lượng                                    Giá", contentFont));
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            for (Detailbookings detailbooking : detailbookings) {
+                Paragraph productDetails = new Paragraph("    " + detailbooking.getBooks().getBookname(), contentFont);
+                productDetails.add(new Paragraph("                " + detailbooking.getQuantity() + "                                         " + detailbooking.getBooks().getPrice()));
+                productDetails.setAlignment(Element.ALIGN_LEFT);
+                document.add(productDetails);
+            }
+
+
+            // Center-align the total cost and thank you message
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            Paragraph totalCostParagraph = new Paragraph("Tổng cộng:                         " + bookings.getCost(), contentFont);
+            totalCostParagraph.setAlignment(Element.ALIGN_CENTER);
+            document.add(totalCostParagraph);
+            document.add(new Paragraph("    ------------------------------------------------------------------    ", contentFont));
+            Paragraph thankYouParagraph = new Paragraph("Cảm ơn quý khách đã mua hàng!", contentFont);
+            thankYouParagraph.setAlignment(Element.ALIGN_CENTER);
+            document.add(thankYouParagraph);
+
+            document.close();
+            byte[] pdfContents = baos.toByteArray();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType("application/pdf"));
+            headers.setContentDispositionFormData("inline", "example.pdf");
+            return new ResponseEntity<>(pdfContents, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
