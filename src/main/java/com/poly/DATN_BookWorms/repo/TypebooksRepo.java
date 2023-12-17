a53ab58cf08cefbda5ec4ed015a32cba344197fd
Đỗ Quang Tùng <99242911+tungcfpro@users.noreply.github.com>
2023-12-18 04:25:25 +0700

    push shipping complete

--- src/main/java/com/poly/DATN_BookWorms/repo/TypebooksRepo.java
+++ src/main/java/com/poly/DATN_BookWorms/repo/TypebooksRepo.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.poly.DATN_BookWorms.entities.Categories;
 import com.poly.DATN_BookWorms.entities.Typebooks;
 
-public interface TypebooksRepo extends JpaRepository<Typebooks, Integer>{
+public interface    TypebooksRepo extends JpaRepository<Typebooks, Integer>{
 
         @Query("Select t.categories from Typebooks t where t.bookid in (Select b.bookid from Books b where b.shopid = ?1)")
         List<Categories> listCategoriesByType(Integer shopid);
