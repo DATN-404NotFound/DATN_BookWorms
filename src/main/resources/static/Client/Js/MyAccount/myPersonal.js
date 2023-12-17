30b94209f7f4e9edf3bd14cf5b74cbc4cce5171b
Camvy050810 <92627708+Camvy050810@users.noreply.github.com>
2023-12-17 14:59:54 +0700

    update 17/12

--- src/main/resources/static/Client/Js/MyAccount/myPersonal.js
+++ src/main/resources/static/Client/Js/MyAccount/myPersonal.js
@@ -1,10 +1,10 @@
 
-function inputFullname(){
+function inputFullname() {
     var inputName = document.getElementById('textName');
     inputName.removeAttribute("disabled");
     inputName.classList.add('inputMypersonal');
 }
-function inputEmail(){
+function inputEmail() {
     var inputEmails = document.getElementById('textEmail');
     inputEmails.removeAttribute("disabled");
     inputEmails.classList.add('inputMypersonal');
@@ -24,3 +24,13 @@ function previewImage(event) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+var currentDate = new Date();
+
+// Giới hạn ngày đến năm 2023
+var maxDate = new Date(2023, 11, 31); // Tháng 11 (0-11), ngày 31
+
+// Chuyển định dạng ngày thành chuỗi YYYY-MM-DD
+var maxDateString = maxDate.toISOString().split('T')[0];
+
+// Thiết lập thuộc tính max cho trường ngày
+document.getElementById('inpDate').setAttribute('max', maxDateString);
